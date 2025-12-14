e3b8c3c04d1e268a08521a40ef000d69051b70f4
pedroaal <pedroaal04@gmail.com>
2025-12-14 15:20:58 -0500

    added base component

---
+++ src/components/core/BlueBoard.tsx
@@ -0,0 +1,89 @@
+import { A } from "@solidjs/router";
+import { FaSolidChevronRight } from "solid-icons/fa";
+import type { JSX, ParentComponent } from "solid-js";
+import { For } from "solid-js";
+import type { Modals } from "~/config/modals";
+import type { Routes } from "~/config/routes";
+import { useApp } from "~/context/app";
+
+interface IAction {
+	label: string;
+}
+
+interface IButton extends IAction {
+	onClick?: () => void;
+}
+
+interface ILink extends IAction {
+	href: Routes;
+}
+
+interface IModal extends IAction {
+	key: Modals;
+}
+
+interface IProps {
+	title?: string | JSX.Element;
+	class?: string;
+	actions?: IButton[];
+	links?: ILink[];
+	modals?: IModal[];
+}
+
+const BlueBoard: ParentComponent<IProps> = (props) => {
+	const { openModal } = useApp();
+
+	return (
+		<div class="card bg-base-100 shadow-xl">
+			<div class="bg-primary text-primary-content rounded-t-box p-3 d-print-none">
+				<h2 class="card-title">{props.title}</h2>
+			</div>
+
+			<div class="card-body p-4">{props.children}</div>
+
+			<div class="bg-base-200 rounded-b-box p-2 d-print-none">
+				<div class="flex gap-2">
+					<div class="flex-1 flex gap-2">
+						<For each={props.links ?? []}>
+							{(item) => (
+								<A href={item.href} class="btn btn-primary btn-link btn-sm">
+									{item.label}
+									<FaSolidChevronRight size={16} />
+								</A>
+							)}
+						</For>
+
+						<For each={props.modals ?? []}>
+							{(item) => (
+								<button
+									type="button"
+									class="btn btn-primary btn-link btn-sm"
+									onClick={[openModal, item.key]}
+								>
+									{item.label}
+									<FaSolidChevronRight size={16} />
+								</button>
+							)}
+						</For>
+					</div>
+
+					<div class="flex gap-2">
+						<For each={props.actions ?? []}>
+							{(item) => (
+								<button
+									class="btn btn-primary btn-sm"
+									type="button"
+									onClick={item.onClick}
+								>
+									{item.label}
+								</button>
+							)}
+						</For>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default BlueBoard;

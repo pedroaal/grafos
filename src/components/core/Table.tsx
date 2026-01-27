233c708a33cd2319be8a2bc46f181281785fbe69
pedroaal <pedroaal04@gmail.com>
2026-01-26 21:55:03 -0500

    added sizes

--- src/components/core/Table.tsx
+++ src/components/core/Table.tsx
@@ -9,12 +9,22 @@ interface IHeader {
 interface IProps {
 	headers: Array<IHeader>;
 	footer?: JSX.Element;
+	size?: "xs" | "sm" | "md" | "lg" | "xl";
 }
 
 const Table: ParentComponent<IProps> = (props) => {
 	return (
 		<div class="overflow-x-auto">
-			<table class="table table-zebra w-full">
+			<table
+				class="table table-zebra w-full"
+				classList={{
+					"table-xs": props.size === "xs",
+					"table-sm": props.size === "sm",
+					"table-md": !props.size || props.size === "md",
+					"table-lg": props.size === "lg",
+					"table-xl": props.size === "xl",
+				}}
+			>
 				<thead>
 					<tr>
 						<For each={props.headers}>

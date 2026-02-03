68fbb25d82f48b7b42f24ed48c88c80f3e720bd2
Pedro Altamirano <pedroaal04@gmail.com>
2026-02-03 10:06:06 -0500

    Update features.ts

--- src/services/users/features.ts
+++ src/services/users/features.ts
@@ -3,15 +3,8 @@ import { DATABASE_ID, TABLES } from "~/config/db";
 import { tables } from "~/lib/appwrite";
 import type { Features } from "~/types/appwrite";
 
-export const listFeatures = async (options: {
-	page?: number;
-	perPage?: number;
-}) => {
-	const { page = 1, perPage = 10 } = options;
-	const queries = [
-		Query.limit(perPage),
-		Query.offset((page - 1) * perPage),
-	];
+export const listFeatures = async () => {
+	const queries = [Query.limit(50)];
 
 	const res = await tables.listRows<Features>({
 		databaseId: DATABASE_ID,

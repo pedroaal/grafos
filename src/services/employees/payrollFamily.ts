4e1053cc631e6debe75cf72cbb130f3bc0531a40
Pedro Altamirano <pedroaal04@gmail.com>
2026-02-03 10:00:42 -0500

    Remove pagination from listPayrollFamily function
    Removed pagination options and related logic from listPayrollFamily function.

--- src/services/employees/payrollFamily.ts
+++ src/services/employees/payrollFamily.ts
@@ -5,14 +5,8 @@ import type { PayrollFamily } from "~/types/appwrite";
 
 export const listPayrollFamily = async (options: {
 	payrollId?: string;
-	page?: number;
-	perPage?: number;
 }) => {
-	const { page = 1, perPage = 10 } = options;
-	const queries = [
-		Query.limit(perPage),
-		Query.offset((page - 1) * perPage),
-	];
+	const queries = [];
 	if (options?.payrollId) queries.push(Query.equal("payrollId", options.payrollId));
 
 	const res = await tables.listRows<PayrollFamily>({

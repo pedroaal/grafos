2d16fe56a0355ad45a17e0ff88281dd76ad9851d
pedroaal <pedroaal04@gmail.com>
2026-02-07 00:18:12 -0500

    fix route

---
+++ src/routes/(landing)/[...404].tsx
@@ -0,0 +1,16 @@
+import { Title } from "@solidjs/meta";
+
+const NotFoundPage = () => {
+	return (
+		<>
+			<Title>Not Found - Grafos</Title>
+			<div class="text-center">
+				<h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
+					Not Found
+				</h1>
+			</div>
+		</>
+	);
+};
+
+export default NotFoundPage;
